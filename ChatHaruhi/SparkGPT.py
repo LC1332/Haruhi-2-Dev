379a82c692e71fc34d78749a15554c097163b780
hhhwmws0117 <60333156+hhhwmws0117@users.noreply.github.com>
2023-10-18 14:42:54 +0800

    Add files via upload

--- ChatHaruhi/SparkGPT.py
+++ ChatHaruhi/SparkGPT.py
@@ -1,49 +1,52 @@
-# ErnieGPT.py
-import erniebot 
+# SparkGPT.py
+from . import SparkApi
 #以下密钥信息从os环境获取
 import os
 
-# appid = os.environ['APPID']
-# api_secret = os.environ['APISecret'] 
-# api_key = os.environ['APIKey']
-erniebot.access_token = os.environ["ErnieAccess"]
+appid = os.environ['APPID']
+api_secret = os.environ['APISecret'] 
+api_key = os.environ['APIKey']
 
 from .BaseLLM import BaseLLM
 
     
 
 
-class ErnieGPT(BaseLLM):
+class SparkGPT(BaseLLM):
 
-    def __init__(self,model="ernie-bot",api_type="aistudio"):
-        super(ErnieGPT,self).__init__()
-        if model not in ["ernie-bot", "ernie-bot-turbo", "ernie-vilg-v2", "ernie-text-embedding"]:
-            raise Exception("Unknown Ernie model")
-        # SparkApi.answer =""
-        self.messages = []
-        erniebot.api_type = api_type
+    def __init__(self, model="Spark2.0"):
+        super(SparkGPT,self).__init__()
+        if model == "Spark2.0":
+            self.domain = "generalv2"    # v2.0版本
+            self.Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
+        elif model == "Spark1.5":
+            self.domain = "general"   # v1.5版本
+            self.Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
+        else:
+            raise Exception("Unknown Spark model")
+
+        self.messages = ""
         
 
     def initialize_message(self):
-        self.messages = []
+        self.messages = ""
 
     def ai_message(self, payload):
-        self.messages = self.messages + "AI: " + payload  
+        self.messages = self.messages + "AI: " + payload  + "\n"
 
     def system_message(self, payload):
-        self.messages = self.messages + "System: " + payload 
+        self.messages = self.messages + "System: " + payload + "\n"
 
     def user_message(self, payload):
-        self.messages = self.messages + "User: " + payload  
+        self.messages = self.messages + "User: " + payload  + "\n"
 
     def get_response(self):
         # question = checklen(getText("user",Input))
-        response = erniebot.ChatCompletion.create(model='ernie-bot', messages=[{"role": "user", "content": self.messages}])
-        # message_json = [{"role": "user", "content": self.messages}]
-        # SparkApi.answer =""
-        # SparkApi.main(appid,api_key,api_secret,self.Spark_url,self.domain,message_json)
-        return response["result"]
+        message_json = [{"role": "user", "content": self.messages}]
+        SparkApi.answer =""
+        SparkApi.main(appid,api_key,api_secret,self.Spark_url,self.domain,message_json)
+        return SparkApi.answer
     
     def print_prompt(self):
-        for message in self.messages:
-            print(f"{message['role']}: {message['content']}")
+        # print(type(self.messages))
+        print(self.messages)
